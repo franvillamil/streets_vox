67c89f3166b618987cc2f4d549f6f1509a071ffd
Fran Villamil <franvillamil@gmail.com>
2021-05-24 15:14:27 +0200

    main analyses done - descriptives (and R&R add-ons) left

--- Makefile
+++ Makefile
@@ -4,20 +4,17 @@
 # ------------------------
 # Variables
 
-out_data = dataset/output/data.csv dataset/dataset.Rout
-# out_desc = descriptives/desc.Rout
-# out_lm = lm/lm.Rout
-# out_robust = lm_robust/robust.Rout
-# out_alt = alt_exp/alt.Rout
 raw_streets = download_str/output/V_072001.csv download_str/output/V_012011.csv download_str/output/V_072011.csv download_str/output/V_012012.csv download_str/output/V_072012.csv download_str/output/V_012013.csv download_str/output/V_072013.csv download_str/output/V_012014.csv download_str/output/V_072014.csv download_str/output/V_012015.csv download_str/output/V_072015.csv download_str/output/V_012016.csv download_str/output/V_072016.csv download_str/output/V_012017.csv download_str/output/V_072017.csv download_str/output/V_012018.csv download_str/output/V_072018.csv download_str/output/V_012019.csv download_str/output/V_072019.csv download_str/output/V_012020.csv download_str/output/V_072020.csv
+dataset = dataset/output/data.csv
+out_main_mod = main_models/output/DiD_estimates.pdf main_models/output/tab_did.tex main_models/output/tab_cs.tex
 
 # ------------------------
 # Main recipes
 
-all: download_elec/output/elec.csv $(out_data) #$(out_desc) $(out_lm) $(out_robust) $(out_alt) taskflow
+all: download_elec/output/elec.csv $(dataset) $(out_main_mod)
 
 clean:
-	rm -rvf $(out_data)# $(out_desc) $(out_lm) $(out_robust) $(out_alt)
+	rm -rvf $(dataset)# $(out_desc) $(out_lm) $(out_robust) $(out_alt)
 	rm -rvf */output/*
 
 taskflow:
@@ -32,21 +29,25 @@ wsubdir:
 # Data
 
 $(raw_streets): download_str/download.R
-	mkdir -p $(@D)/output
+	mkdir -p $(@D)
 	Rscript --no-save --verbose $< 2>&1 | tee $<out
 
 str_changes/output/changes.csv: str_changes/str_chg.R $(raw_streets)
-	mkdir -p $(@D)/output
+	mkdir -p $(@D)
 	Rscript --no-save --verbose $< 2>&1 | tee $<out
 
 str_local_vars/output/fs.csv: str_local_vars/local.R input/calles_franquistas.txt str_changes/output/changes.csv $(raw_streets)
-	mkdir -p $(@D)/output
+	mkdir -p $(@D)
 	Rscript --no-save --verbose $< 2>&1 | tee $<out
 
 download_elec/output/elec.csv: download_elec/elec.R
 	mkdir -p $(@D)
 	Rscript --no-save --verbose $< 2>&1 | tee $<out
 
-$(out_data): dataset/dataset.R input/unemployment_01_2019.csv input/unemployment_01_2016.csv input/major_izq_muni.csv input/INE_census.csv str_local_vars/output/fs.csv download_elec/output/elec.csv
-	mkdir -p $(@D)/output
+$(dataset): dataset/dataset.R input/unemployment_01_2019.csv input/unemployment_01_2016.csv input/major_izq_muni.csv input/INE_census.csv str_local_vars/output/fs.csv download_elec/output/elec.csv
+	mkdir -p $(@D)
+	Rscript --no-save --verbose $< 2>&1 | tee $<out
+
+$(out_main_mod): main_models/mod.R main_models/functions_did.R $(dataset)
+	mkdir -p $(@D)
 	Rscript --no-save --verbose $< 2>&1 | tee $<out
---
+++ main_models/mod.R
@@ -0,0 +1,219 @@
+# setwd("~/Documents/Projects/streets_vox")
+options(stringsAsFactors = FALSE)
+# List of packages
+pkg = c("dplyr", "tidyr", "stargazer", "ggplot2", "stringr", "MASS")
+# Checks if they are installed, install if not
+if (length(setdiff(pkg, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(pkg, rownames(installed.packages())))}
+# Load
+lapply(pkg, library, character.only = TRUE)
+
+# ------------------------------
+
+# Load functions
+source("main_models/functions_did.R")
+
+# My Stargazer
+my_stargazer = function(dest_file, model_list,
+  omit, title, label, order, covariate.labels, notes_table, dep.var.labels){
+
+  filecon = file(dest_file)
+  writeLines(
+    stargazer(model_list, omit = omit, title = title, label = label,
+      order = order, covariate.labels = covariate.labels, notes = notes_table,
+      omit.stat = c("f", "ser"),
+      intercept.bottom = FALSE,
+      column.sep.width = "-20pt",
+      multicolumn = FALSE,
+      dep.var.caption = "",
+      dep.var.labels = dep.var.labels,
+      font.size = "small",
+      digits = 3,
+      digits.extra = 0,
+      star.char = c("+", "*", "**", "***"),
+      star.cutoffs = c(0.1, 0.05, 0.01, 0.001),
+      notes.align = "c",
+      align = TRUE,
+      no.space = TRUE,
+      add.lines=list(c("CCAA Fixed Effects", rep("\\multicolumn{1}{c}{Yes}", length(model_list)))),
+      notes.label = "",
+      notes.append = FALSE),
+  filecon)
+  close(filecon)
+
+}
+
+# ------------------------------
+
+# Load data
+data = read.csv("dataset/output/data.csv")
+
+# Prepare
+data = data %>% mutate(
+  l_fs_2001_06 = log(fs_2001_06 + 1),
+  l_fs_2016_06 = log(fs_2016_06 + 1),
+  l_fs_2018_12 = log(fs_2018_12 + 1),
+  l_fs_2019_06 = log(fs_2019_06 + 1),
+  l_fs_rm_2016s2_2018s2 = log(fs_rm_2016s2_2018s2 + 1),
+  change_2019_VOX = VOX2019_11 / VOX2019_04)
+data$change_2019_VOX[data$change_2019_VOX == "Inf"] = NA
+
+# Long-form data for the DiD analyses
+# VOX
+dl_VOX = data %>%
+  filter(!is.na(VOX2016_06) & fs_2016_06 > 0) %>%
+  dplyr::select(-ends_with(c("2011_11", "2019_11"))) %>%
+  pivot_longer(
+    cols = starts_with("VOX"),
+    names_to = "election",
+    names_prefix = "VOX",
+    values_to = c("VOX_share")) %>%
+  mutate(VOX_share = VOX_share * 100) %>%
+  as.data.frame()
+
+# PP
+dl_PP = data %>%
+  filter(fs_2016_06 > 0) %>%
+  dplyr::select(-ends_with(c("2019_11"))) %>%
+  pivot_longer(
+    cols = starts_with("PP"),
+    names_to = "election",
+    names_prefix = "PP",
+    values_to = c("PP_share")) %>%
+  mutate(PP_share = PP_share * 100) %>%
+  as.data.frame()
+
+# PSOE
+dl_PSOE = data %>%
+  filter(fs_2016_06 > 0) %>%
+  dplyr::select(-ends_with(c("2019_11"))) %>%
+  pivot_longer(
+    cols = starts_with("PSOE"),
+    names_to = "election",
+    names_prefix = "PSOE",
+    values_to = c("PSOE_share")) %>%
+  mutate(PSOE_share = PSOE_share * 100) %>%
+  as.data.frame()
+
+# ------------------------------
+# Cross-sectional models
+
+
+m_cs1 = lm(VOX2019_04 ~ fs_rm_2001s2_2018s2_bin +
+  unemp_2019 + part2019_04 + lpop2011 + factor(ccaa),
+  data = data)
+
+m_cs2 = lm(VOX2019_11 ~ fs_rm_2001s2_2018s2_bin +
+  unemp_2019 + part2019_11 + lpop2011 + factor(ccaa),
+  data = data)
+
+m_cs3 = lm(VOX2019_04 ~ fs_rm_2001s2_2018s2_bin +
+  unemp_2019 + part2019_04 + lpop2011 + factor(ccaa),
+  data = subset(data, fs_2001_06 > 0))
+
+m_cs4 = lm(VOX2019_11 ~ fs_rm_2001s2_2018s2_bin +
+  unemp_2019 + part2019_11 + lpop2011 + factor(ccaa),
+  data = subset(data, fs_2001_06 > 0))
+
+
+my_stargazer(dest_file = "main_models/output/tab_cs.tex",
+  model_list = list(m_cs1, m_cs2, m_cs3, m_cs4),
+  omit = "ccaa",
+  label = "tab:cs",
+  title = "Francoist street name removal and electoral support for Vox",
+  order = c("Constant"),
+  dep.var.labels = c("\\footnotesize Apr 2019", "\\footnotesize Nov 2019",
+    "\\footnotesize Apr 2019", "\\footnotesize Nov 2019"),
+  covariate.labels = c("(Intercept)",
+    "Francoist street name removal",
+    "Unemployment 2019",
+    "Turnout April 2019",
+    "Turnout Nov 2019",
+    "Log. Population"),
+  notes_table = "\\parbox[t]{0.8\\textwidth}{\\textit{Note:} $+ p<0.1; * p<0.05; ** p<0.01; *** p<0.001$. The main independent variable refers to the removal of Francoist street names between June 2001 and December 2018. Models 3 and 4 only include municipalities that had Francoist street names in June 2001.}")
+
+
+# ------------------------------
+# Difference-in-Differences
+
+did_VOX1 = lm(VOX_share ~ fs_rm_2016s2_2018s2_bin * factor(election) +
+  major_2015_izq + lpop2011 + l_fs_2016_06 + unemp_2016 + factor(ccaa),
+  data = subset(dl_VOX, election %in% c("2016_06", "2019_04")))
+
+did_PP1 = lm(PP_share ~ fs_rm_2016s2_2018s2_bin * factor(election) +
+  major_2015_izq + lpop2011 + l_fs_2016_06 + unemp_2016 + factor(ccaa),
+  data = subset(dl_PP, election %in% c("2016_06", "2019_04")))
+
+did_PSOE1 = lm(PSOE_share ~ fs_rm_2016s2_2018s2_bin * factor(election) +
+  major_2015_izq + lpop2011 + l_fs_2016_06 + unemp_2016 + factor(ccaa),
+  data = subset(dl_PSOE, election %in% c("2016_06", "2019_04")))
+
+my_stargazer(dest_file = "main_models/output/tab_did.tex",
+  model_list = list(did_VOX1, did_PP1, did_PSOE1),
+  omit = "ccaa",
+  label = "tab:main_did",
+  title = "Francoist street name removal and increase in electoral support for parties",
+  dep.var.labels = c("VOX", "PP", "PSOE"),
+  order = c("Constant"),
+  covariate.labels = c("(Intercept)",
+    "Francoist street name removal",
+    "Election April 2019",
+    "Francoist removal $\\times$ April 2019",
+    "Leftist major 2015", "Log. Population",
+    "Log. No. Francoist streets $t_{0}$",
+    "Unemployment"),
+  notes_table = "\\parbox[t]{0.75\\textwidth}{\\textit{Note:} $+ p<0.1; * p<0.05; ** p<0.01; *** p<0.001$. Only municipalities that had at least one street with a Francoist name in $t_{0}$ were included in the sample.}")
+
+
+# ------------------------------
+# Simulation (DiD)
+
+# Extra variables for simulation
+extra_vars = data.frame(major_2015_izq = 0,
+  lpop2011 = mean(data$lpop2011, na.rm = T),
+  l_fs_2016_06 = mean(data$l_fs_2016_06, na.rm = T),
+  unemp_2016 = mean(data$unemp_2016, na.rm = T))
+
+# Setting a seed
+set.seed(304261)
+
+# Simulating each model and obtaining estimates
+sim_VOX = did_sim(m = did_VOX1,
+  fs_chg_var = "fs_rm_2016s2_2018s2_bin",
+  other_vars = extra_vars, FE = "ccaa")
+sim_VOX_est = calculate_did_estimate(depvar_label = "VOX",
+  simulations = sim_VOX, elec_t0 = "2016_06", elec_t1 = "2019_04")
+
+sim_PP = did_sim(m = did_PP1,
+  fs_chg_var = "fs_rm_2016s2_2018s2_bin",
+  other_vars = extra_vars, FE = "ccaa")
+sim_PP_est = calculate_did_estimate(depvar_label = "PP",
+  simulations = sim_PP, elec_t0 = "2016_06", elec_t1 = "2019_04")
+
+sim_PSOE = did_sim(m = did_PSOE1,
+  fs_chg_var = "fs_rm_2016s2_2018s2_bin",
+  other_vars = extra_vars, FE = "ccaa")
+sim_PSOE_est = calculate_did_estimate(depvar_label = "PSOE",
+  simulations = sim_PSOE, elec_t0 = "2016_06", elec_t1 = "2019_04")
+
+# Putting all together
+sim = rbind(sim_VOX_est, sim_PP_est, sim_PSOE_est)
+
+# DiD estimates plot
+pdf("main_models/output/DiD_estimates.pdf", width = 5.5, height = 3)
+ggplot(sim, aes(x = depvar, y = mean)) +
+  geom_point(shape = 1, size = 2) +
+  geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0) +
+  geom_errorbar(aes(ymin = lwr90, ymax = upr90), size = 1.1, width = 0) +
+  geom_text(aes(label = round(mean, 2)), nudge_x = 0.15, size = 3) +
+  theme_classic() +
+  theme(axis.text.y = element_text(size = 12),
+    panel.background = element_blank(),
+    panel.grid = element_blank(),
+    # panel.border = element_blank(),
+    axis.ticks.y = element_blank()) +
+  geom_hline(yintercept = 0, linetype = "dotted") +
+  labs(x = "",
+    y = "\nIncrease in (%) vote share 2016-2019 due to\nFrancoist street name removal") +
+  coord_flip()
+dev.off()
